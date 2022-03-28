bdc72b762f0007afea90ca4f3d2daeb542e608e9
Venkatasurysai <venkatasuryasaip927@gmail.com>
2022-03-28 15:56:53 +0530

    Examples of array

---
+++ arraydemo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cnt=0;
+fruits[((cnt++))]="Mangoes";
+fruits[((cnt++))]="Watermelon";
+fruits[((cnt++))]="Grapes";
+fruits[((cnt++))]="Bananas";
+fruits[((cnt++))]="Oranges";
+fruits[((cnt++))]="Apple";
+
+echo ${fruits[@]};
+
+echo ${fruits[1]};
+
+echo "Size of an array is  : " ${#fruits[@]}
